311afc82349131cb385ccd2277c1ecdcc4b368a5
Mayur <mayur.jaganmohan@gmail.com>
2020-05-09 15:45:18 +0530

    Updates

---
+++ precache-manifest.50c3e37b71dfac8b85ff3d955e912b9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c7e0be3eb80d6337faee1eafbb9409",
+    "url": "/githubpages-test/index.html"
+  },
+  {
+    "revision": "15c2f834cd973f390647",
+    "url": "/githubpages-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/githubpages-test/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/githubpages-test/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c2f834cd973f390647",
+    "url": "/githubpages-test/static/js/main.6498234a.chunk.js"
+  },
+  {
+    "revision": "e62c62df28faf491fa4d",
+    "url": "/githubpages-test/static/js/runtime-main.77eaf1e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubpages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
